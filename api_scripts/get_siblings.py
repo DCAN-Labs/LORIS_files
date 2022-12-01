ba573176df0215a3096b4eb0513db2008eeb58fa
Barry Tikalsky <tikal004@umn.edu>
2022-12-01 14:57:23 -0600

    preliminary sibling data transfer function

--- api_scripts/get_siblings.py
+++ api_scripts/get_siblings.py
@@ -41,7 +41,46 @@ def get_subproject_function(subject):
         print("Invalid Subject ID for get_subproject_function")
         return -1
 
-loris.populate_session_table_override(visits=visits, override_visits=override_visits, get_subproject_function=get_subproject_function)
+# loris.populate_session_table_override(visits=visits, override_visits=override_visits, get_subproject_function=get_subproject_function)
+
+tests = {
+    "srs2_schoolage": "srs_school_age_dob",
+    'srs2_preschool': "dob_srs_presc",
+    'srs2_relative_other': "dob_srs_adult_other",
+    'aq_ages_1215': "aqt_dob",
+    'aq_ages_16': "aqa_dob",
+    'aq_ages_411': "aqc_dob",
+    'cbcl_ages_15_to_5': "cbcl_1_dob",
+    'cbcl_ages_6': "cbcl_dob",
+    'conners_3': "con3_p_dob"
+}
+
+def handle_subject_ids(subject):
+    return subject[:12]
+
+keys = list(loris.records[0].keys())
+multi_selects = []
+for key in keys:
+    if "___" in key:
+        split_key = key.split("___")[0]
+        if split_key not in multi_selects:
+            multi_selects.append(split_key)
+
+num_flag = loris.query(table="flag", fields=["count(*)"])[0]["count(*)"]
+updated_inst = 0
+updated_flag = 0
+num_error = 0
+
+for record in loris.records:
+    if record['sib_1_dob']:
+        sibling_records = [subject_record for subject_record in loris.records if record["record_id"] == subject_record["record_id"] and subject_record["redcap_repeat_instrument"] in tests.keys()]
+        for sibling_record in sibling_records:
+            for visit in override_visits:
+                for test, dob_field in tests.items():
+                    if record[visit["identifier"]] == sibling_record[dob_field] and sibling_record[dob_field] != "":
+                        updated_inst, updated_flag, num_error = loris.update_data(handle_subject_ids=handle_subject_ids, record=sibling_record, multi_selects=multi_selects, visit_label=visit["label"], updated_inst=updated_inst, updated_flag=updated_flag, num_error=num_error)
+
+print(f"{num_flag} tests in flag. {updated_inst} instrument entries updated. {updated_flag} flag entries updated. {num_error} errors.")
 
 loris.commit()
 
